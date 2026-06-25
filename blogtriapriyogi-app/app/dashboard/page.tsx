62c86ac70785c0d7b8a0b703b4bdcc2b23f7f531
TRI APRIYOGI BAHARI <triapriyogibahari34@gmail.com>
2026-06-25 06:09:59 +0000

    fix headers async promise issue nextjs

--- blogtriapriyogi-app/app/dashboard/page.tsx
+++ blogtriapriyogi-app/app/dashboard/page.tsx
@@ -1,12 +1,12 @@
-import { headers } from 'next/headers';
+import { headers } from 'next/headers'
 
 export default async function Page() {
-  const h = await headers();
-  const tenant = h.get('x-tenant');
+  const h = await headers()
+  const tenant = h.get('x-tenant')
 
   return (
     <div style={{ padding: 20 }}>
       Dashboard Tenant: {tenant}
     </div>
-  );
+  )
 }
